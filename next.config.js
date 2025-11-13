eaa4fc04b1bf86de1944e58406389092f421d83a
xavier_haurie <xavier.haurie@gmail.com>
2025-11-12 22:15:48 -0500

    Fix build for prod deploy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ import path from "node:path";
 const nextConfig = {
     webpack: (config) => {
         // Ensure alias works even if tsconfig paths aren’t picked up
-        config.resolve.alias["@"] = path.resolve(process.cwd());
+        config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
         return config;
     },
     reactStrictMode: true,
