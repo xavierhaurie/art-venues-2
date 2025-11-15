ce0da42cfa44905654abd44ffda2905e8d8215be
xavier_haurie <xavier.haurie@gmail.com>
2025-11-15 00:44:00 -0500

    Fix build for prod deploy

--- src/app/venues/page.tsx
+++ src/app/venues/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
-import VenueStickers from '@/components/VenueStickers';
+import VenueStickers from '@/components/VenueStickers.tsx';
 import VenueModal from "@/components/VenueModal";
 import LocalityPickerModal from '@/components/LocalityPickerModal';
 import VenueTypePickerModal from '@/components/VenueTypePickerModal';
