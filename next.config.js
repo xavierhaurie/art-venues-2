53c1428e9b6ebd88febbc292a57e179682656477
xavier_haurie <xavier.haurie@gmail.com>
2025-11-15 22:44:35 -0500

    Fix build for prod deploy

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         // Map `@` to `src` so `@/components/...` works in all environments
         config.resolve.alias = {
             ...(config.resolve.alias || {}),
-            "@": path.resolve(process.cwd(), "src"),
+            "@": path.resolve(process.cwd(), ""),
         };
         return config;
     },
