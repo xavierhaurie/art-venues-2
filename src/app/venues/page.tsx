514ff690c13e3d0b5f1eb9d9eaf3e55db3e239ee
xavier_haurie <xavier.haurie@gmail.com>
2025-11-15 22:28:22 -0500

    Fix build for prod deploy

--- src/app/venues/page.tsx
+++ src/app/venues/page.tsx
@@ -2,15 +2,15 @@
 
 import React, { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
-import VenueStickers from 'components/VenueStickers';
+import VenueStickers from '/components/VenueStickers';
 import VenueModal from "/components/VenueModal";
-import LocalityPickerModal from 'src/components/LocalityPickerModal';
-import VenueTypePickerModal from 'src/components/VenueTypePickerModal';
-import StickerPickerModal from 'src/components/StickerPickerModal';
-import OtherFiltersModal from 'src/components/OtherFiltersModal';
-import FeedbackModal from 'src/components/FeedbackModal';
-import { useVenueStore } from 'src/lib/store/venueStore';
-import { useConfigStore } from 'src/lib/store/configStore';
+import LocalityPickerModal from '/components/LocalityPickerModal';
+import VenueTypePickerModal from '/components/VenueTypePickerModal';
+import StickerPickerModal from '/components/StickerPickerModal';
+import OtherFiltersModal from '/components/OtherFiltersModal';
+import FeedbackModal from '/components/FeedbackModal';
+import { useVenueStore } from '/lib/store/venueStore';
+import { useConfigStore } from '/lib/store/configStore';
 
 interface Venue {
   id: string;
