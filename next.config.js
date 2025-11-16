b8b09182cfa74b832a83ce3d1ffba7ac9b12fbf2
xavier_haurie <xavier.haurie@gmail.com>
2025-11-15 22:59:08 -0500

    Revert "Fix build for prod deploy"
    This reverts commit 53c1428e9b6ebd88febbc292a57e179682656477.

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         // Map `@` to `src` so `@/components/...` works in all environments
         config.resolve.alias = {
             ...(config.resolve.alias || {}),
-            "@": path.resolve(process.cwd(), ""),
+            "@": path.resolve(process.cwd(), "src"),
         };
         return config;
     },
